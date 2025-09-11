6c0d671425bf95e60ab1b7e43c9a47f0c57b5bf5
apl888 <aploria@gmail.com>
2025-09-11 16:04:24 -0500

    edits

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -64,8 +64,8 @@ class GasPreprocessor:
         plt.close('all') # close any open figures to avoid ghost plots
     
         plt.figure(figsize=figsize)
-        plt.plot(raw_series, label='Raw Data', marker='.', markersize=3, color='black', linewidth=0.75, alpha=0.5)
-        plt.plot(processed_series, label='Smoothed, Resampled, & Interpolated', color='red', alpha=0.7)
+        plt.plot(raw_series, label='Raw Data', marker='.', markersize=3, color='#0072B2', linewidth=0.75, alpha=0.5)
+        plt.plot(processed_series, label='Smoothed, Resampled, & Interpolated', color='#E69F00', alpha=0.7)
         
         title = custom_title if custom_title else f'{self.gas_name} Time Series'
         plt.title(title, fontsize=title_fontsize)

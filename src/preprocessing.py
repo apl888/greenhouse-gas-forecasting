45a3cc844614a1a3eb53aa76fde0d6c59aeff693
apl888 <aploria@gmail.com>
2025-09-23 15:48:20 -0500

    edits

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -555,15 +555,15 @@ class GasPreprocessor:
         
         plt.subplot(1,2,1)
         plot_acf(series_clean, ax=plt.gca(), lags=safe_lags)
-        plt.title(f'ACF Plot of {title_suffix}', fontsize=16)
-        plt.ylabel('Autocorrelation Coef', fontsize=16, y=1.10)
+        plt.title(f'ACF Plot of {title_suffix}', fontsize=16, y=1.10)
+        plt.ylabel('Autocorrelation Coef', fontsize=16)
         plt.xlabel('Lag', fontsize=16)
         plt.yticks(fontsize=14)
         plt.xticks(fontsize=14)
         
         plt.subplot(1,2,2)
         plot_pacf(series_clean, ax=plt.gca(), lags=safe_lags)
-        plt.title(f'PACF Plot of {title_suffix}', fontsize=16)
+        plt.title(f'PACF Plot of {title_suffix}', fontsize=16, y=1.10)
         plt.ylabel('Partial Autocorrelation Coef', fontsize=16)
         plt.xlabel('Lag', fontsize=16)
         plt.yticks(fontsize=14)

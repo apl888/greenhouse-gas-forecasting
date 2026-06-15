cc44a0033d3eddd6df30de067374aaddfd4c0495
apl888 <aploria@gmail.com>
2026-06-15 13:34:41 -0500

    edits

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -54,7 +54,7 @@ class GasPreprocessor:
                  window=1, 
                  iqr_factor=1.5, 
                  interpolate_method='linear', 
-                 lags=104, 
+                 lags=52, 
                  do_eda=False, 
                  transformation=None, 
                  bc_lambda=None):

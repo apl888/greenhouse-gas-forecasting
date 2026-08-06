22bb8c17e25ca67de71f812f66b3fe5020de08ae
apl888 <aploria@gmail.com>
2026-08-06 11:29:33 -0500

    edits

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1243,13 +1243,13 @@ def rolling_crps(
         for i, h in enumerate(horizons):
             z = (y_true[i] - mu_h[i]) / sigma_h[i]
             
-            if h == 52 and t > len(y) - 55:
-                print()
-                print("t =", t)
-                print("origin =", y.index[t])
-                print("y_test first =", y_test.index[0])
-                print("y_test last  =", y_test.index[-1])
-                print("stored y_true =", y_true[i])
+            # if h == 52 and t > len(y) - 55:
+            #     print()
+            #     print("t =", t)
+            #     print("origin =", y.index[t])
+            #     print("y_test first =", y_test.index[0])
+            #     print("y_test last  =", y_test.index[-1])
+            #     print("stored y_true =", y_true[i])
                 
             records.append({
                 'origin'            : y.index[t],
